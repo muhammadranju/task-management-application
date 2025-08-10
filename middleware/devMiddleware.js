bc194b6c14efecbd25cb0360a1bc31d1eca89819
Muhammad Ranju <mdranju23@gmail.com>
2025-08-11 02:22:28 +0600

    feat: update cors access url

--- middleware/devMiddleware.js
+++ middleware/devMiddleware.js
@@ -5,7 +5,7 @@ const middleware = [
   express.json(),
   express.urlencoded({ extended: true }),
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: ["http://localhost:5173", "https://task-manager-9f8db.web.app"],
     credentials: true,
   }),
   morgan("dev"),
